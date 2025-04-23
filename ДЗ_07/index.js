b52ef8a531415d86bb6fc7cf6387c3c5e6174962
Catsack <tnnic82@gmail.com>
2025-04-23 16:44:36 +0300

    исправил последнее задание

--- ДЗ_07/index.js
+++ ДЗ_07/index.js
@@ -104,17 +104,32 @@ function currySum(a) {
 // завершения. Таймер движется назад.При достижении точки
 // завершения в консоль выводится значение таймера и
 // сообщение о завершении работы таймера.
-function timer(start, end) {
+// function timer(start, end) {
+//   let current = start;
+
+//   const intervalId = setInterval(() => {
+//     console.log(current);
+//     if (current <= end) {
+//       clearInterval(intervalId);
+//       console.log('Таймер завершил работу.');
+//     }
+//     current--;
+//   }, 1000);
+// }
+
+// timer(10, 0); // Счёт от 10 до 0
+function createTimer(start, end) {
   let current = start;
 
-  const intervalId = setInterval(() => {
-    console.log(current);
-    if (current <= end) {
-      clearInterval(intervalId);
-      console.log('Таймер завершил работу.');
-    }
-    current--;
-  }, 1000);
+  // ЯВНОЕ ЗАМЫКАНИЕ: возвращается функция, которая хранит current и end
+  return function () {
+    const intervalId = setInterval(() => {
+      console.log(current);
+      if (current <= end) {
+        clearInterval(intervalId);
+        console.log("Таймер завершил работу.");
+      }
+      current--;
+    }, 1000);
+  };
 }
-
-timer(10, 0); // Счёт от 10 до 0
--- ДЗ_07/timer.js
+++ ДЗ_07/timer.js
@@ -1,14 +1,32 @@
-function timer(start, end) {
+// function timer(start, end) {
+//   let current = start;
+
+//   const intervalId = setInterval(() => {
+//     console.log(current);
+//     if (current <= end) {
+//       clearInterval(intervalId);
+//       console.log("Таймер завершил работу.");
+//     }
+//     current--;
+//   }, 1000);
+// }
+
+// timer(10, 0); // Счёт от 10 до 0
+function createTimer(start, end) {
   let current = start;
 
-  const intervalId = setInterval(() => {
-    console.log(current);
-    if (current <= end) {
-      clearInterval(intervalId);
-      console.log("Таймер завершил работу.");
-    }
-    current--;
-  }, 1000);
+  // ЯВНОЕ ЗАМЫКАНИЕ: возвращается функция, которая хранит current и end
+  return function () {
+    const intervalId = setInterval(() => {
+      console.log(current);
+      if (current <= end) {
+        clearInterval(intervalId);
+        console.log("Таймер завершил работу.");
+      }
+      current--;
+    }, 1000);
+  };
 }
 
-timer(10, 0); // Счёт от 10 до 0
+const startTimer = createTimer(10, 5); // Возвращает функцию-таймер
+startTimer(); // Запускаем
